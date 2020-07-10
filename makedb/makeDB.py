db6f60dcf82c7bcd3157877d70de9287b717a2d9
Chaim Stanton <38401049+ChaimStanton@users.noreply.github.com>
2020-07-10 02:56:19 +0100

    itNowOutputstoDB

--- makedb/makeDB.py
+++ makedb/makeDB.py
@@ -14,9 +14,20 @@ def getTwitterID(name):
     user = api.get_user(name)
     return user.id_str
 
+class MP():
+    def __init__(self, handleWat):
+        self.handleWat = handleWat
+        self.id_str = getTwitterID(self.handleWat)
+
+db = TinyDB("db/data.json")
+
+MPs = [MP("@ChaimStanton")]
+
+for MP in MPs:
+    db.insert({'Handle': MP.handleWat, "id_str": MP.id_str})
 
 db = TinyDB("db/data.json")
 User = Query()
 getTwitterID("ChaimStanton")
 
-# db.insert({'name': 'John', "twitter_handle": , 'twitter_id_str': 22})
+db.insert({'name': 'John', "twitter_handle": 5, 'twitter_id_str': 22})
