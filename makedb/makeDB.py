a1c54952c779943391979be43091a89da7c9fe52
Chaim Stanton <38401049+ChaimStanton@users.noreply.github.com>
2020-08-18 01:08:52 +0100

    added option to make a keys database

--- makedb/makeDB.py
+++ makedb/makeDB.py
@@ -4,6 +4,7 @@ import tweepy
 
 from tinydb import TinyDB, Query
 from keysAuthentication import *
+from time import sleep
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
@@ -11,23 +12,83 @@ auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
 def getTwitterID(name):
-    user = api.get_user(name)
-    return user.id_str
+    try:
+        user = api.get_user(name)
+        return user.id_str
+    except tweepy.error.TweepError:
+        print("User" + name + "not found")
+        return 1
+
 
 class MP():
     def __init__(self, handleWat):
         self.handleWat = handleWat
-        self.id_str = getTwitterID(self.handleWat)
+        self.id_str = getTwitterID(handleWat)
 
-db = TinyDB("db/data.json")
+while True:
+    print("""
+    1 - Make the database of MP's from a csv
+    2 - Enter a handle get the ID
+    3 - Make keys authentication database
+    9 - Exit
+    """)
+    choice = input("What is your selection: ")
+    if choice == "1":
+        politicalParty = input("Enter the political party: ")
+        db = TinyDB("db/" + politicalParty + ".json")
+        print("""
+Save your file in the input_data as a csv
+save it in the input_data folder
+        """)
+        fileName = input("Input the name: ")
 
-MPs = [MP("@ChaimStanton")]
+        try: 
+            with open("input_data/"+ fileName, "r") as data:
+                dataList = data.read().splitlines()
+        except FileNotFoundError:
+            print("File NOT found")
+            sleep(1.5)
+            continue # restarts the while loop
 
-for MP in MPs:
-    db.insert({'Handle': MP.handleWat, "id_str": MP.id_str})
+        print(dataList)
 
-db = TinyDB("db/data.json")
-User = Query()
-getTwitterID("ChaimStanton")
+        MPs = []
+        for MPstrHandle in dataList:
+            MPs.append(MP(MPstrHandle))
 
-db.insert({'name': 'John', "twitter_handle": 5, 'twitter_id_str': 22})
+        for MP in MPs:
+            db.insert({'Handle': MP.handleWat, "id_str": MP.id_str})
+
+    elif choice == "2":
+        try:
+            name = input("Enter the username: ")
+            print(getTwitterID(name))
+        except tweepy.error.TweepError:
+            print("Invalid username")
+
+    elif choice == "3":
+        print("Entering keys authentication creation part")
+        politicalParty = input("Enter the political party: ")
+
+        db = TinyDB("db/" + politicalParty + "KEYS.json")
+
+        consumer_key = input("Enter the consumer_key : ")
+        consumer_secret = input("Enter the consumer_secret : ")
+        access_token = input("Enter the access_token : ")
+        access_token_secret = input("Enter the access_token_secret : ")
+
+        print("Creating database now .... ")
+        sleep(1)
+        
+        db.insert({'consumer_key': consumer_key})
+        db.insert({"consumer_secret": consumer_secret})
+        db.insert({"access_token" : access_token})
+        db.insert({"access_token_secret" : access_token_secret})
+        
+        print("Done")
+
+    elif choice == "9":
+        print("Thank you goodbye")
+        quit()
+    else:
+        print("Invalid choice")
